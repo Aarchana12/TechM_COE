3081f492a448c8b633f3e19a92fda17fd296d280
Aarchana Nichani <aarchananichani12@gmail.com>
2025-02-18 14:13:03 +0530

    Create mul_table.js

---
+++ JAVA_HANDS-ON/JavaScript_Examples/mul_table.js
@@ -0,0 +1,10 @@
+let number = 5; // Example number
+let limit = 10; // Example limit
+
+function displayMultiplicationTable() {
+  for (let i = 1; i <= limit; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+  }
+}
+
+displayMultiplicationTable();
