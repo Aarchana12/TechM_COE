bd030d9fe30410982a864eb722ca496601c98100
Aarchana Nichani <aarchananichani12@gmail.com>
2025-02-18 14:07:10 +0530

    Create var.js

---
+++ JAVA_HANDS-ON/JavaScript_Examples/var.js
@@ -0,0 +1,8 @@
+var num = -5; // Example number
+if (num > 0) {
+  console.log("The number is positive.");
+} else if (num < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
