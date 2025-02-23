295d60b573e4bca7c1a7183ea38fd905c0fd8705
Aarchana Nichani <aarchananichani12@gmail.com>
2025-02-23 23:51:32 +0530

    Create SearchElement.java

---
+++ JAVA_CONSOLIDATED_TASKS/TASK_7(COLLECTIONS API)/SearchElement.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class SearchElement {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(Arrays.asList("Red", "Blue", "Green", "Yellow", "Purple"));
+        System.out.println(colors.contains("Blue"));
+    }
+}
