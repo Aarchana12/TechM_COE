722a837ce34f7dde990f7da97ee0bec1b8b9a198
Aarchana Nichani <aarchananichani12@gmail.com>
2025-02-23 23:55:01 +0530

    Delete JAVA_CONSOLIDATED_TASKS/TASK_7 directory

--- JAVA_CONSOLIDATED_TASKS/TASK_7/AppendToLinkedList.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class AppendToLinkedList {
-    public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<>(Arrays.asList("Red", "Blue", "Green"));
-        list.add("Yellow");
-        System.out.println(list);
-    }
-}
