1cf564b5166a6180359602502ec0224859262d2c
Aarchana Nichani <aarchananichani12@gmail.com>
2025-02-27 13:58:06 +0530

    Create C1.jsx

---
+++ JAVA_HANDS-ON/REACT_HANDS_ON/ReactNestedImports/src/C1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import C2 from './f1/C2';
+
+function C1() {
+    return (
+        <div>
+            <h2>Component 1</h2>
+            <C2 />
+        </div>
+    );
+}
+
+export default C1;
